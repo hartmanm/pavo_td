f661e35a0d491444fe2d7620b60f5ede2fe65939
hartmanm <hartmami@oregonstate.edu>
2017-04-23 19:29:07 -0400

    trimmed and minor changes

--- pavo_td/public/game/levelOneEngine.js
+++ pavo_td/public/game/levelOneEngine.js
@@ -4,7 +4,7 @@
         //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
         //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
 
-        var game = new Phaser.Game(1000, 1000, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+        var game = new Phaser.Game(800, 800, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 // var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
@@ -15,22 +15,28 @@ function preload() {
 
 var map;
 var layer1;
-var layer2;
-var layer3;
+var layer;
+//var layer2;
+//var layer3;
 
 var marker;
 var currentTile = 0;
 var currentLayer;
 
 var cursors;
-var showLayersKey;
-var layer1Key;
-var layer2Key;
-var layer3Key;
+//var showLayersKey;
+//var layer1Key;
+//var layer2Key;
+//var layer3Key;
+//var larger;
 
 function create() {
 
     game.stage.backgroundColor = '#2d2d2d';
+    
+   // larger = game.add.tileSprite(0, 0, 800, 800)
+   
+   // map = game.add.tilemap('map', 16, 16);
 
     //  Creates a blank tilemap
     map = game.add.tilemap();
@@ -41,19 +47,25 @@ function create() {
     //  Creates a new blank layer and sets the map dimensions.
     //  In this case the map is 40x30 tiles in size and the tiles are 32x32 pixels in size.
     layer1 = map.create('level1', 60, 60, 32, 32);
-    layer1.scrollFactorX = 0.5;
-    layer1.scrollFactorY = 0.5;
+    
+   //  var layer1 = window._layer = map.createLayer('Bottom', 1800, 1800);
+  //  layer1.scrollFactorX = 0.5;
+ //   layer1.scrollFactorY = 0.5;
+  //  layer = map.createLayer('lvl1');
 
     //  Resize the world
-    layer1.resizeWorld();
+ //   layer.resizeWorld();
+    
+    
+  //  layer1.debug = true;
 
-    layer2 = map.createBlankLayer('level2', 60, 60, 32, 32);
-    layer2.scrollFactorX = 0.8;
-    layer2.scrollFactorY = 0.8;
+  //  layer2 = map.createBlankLayer('level2', 40, 40, 32, 32);
+  //  layer2.scrollFactorX = 0.8;
+  //  layer2.scrollFactorY = 0.8;
 
-    layer3 = map.createBlankLayer('level3', 60, 60, 32, 32);
+  //  layer3 = map.createBlankLayer('level3', 40, 40, 32, 32);
 
-    currentLayer = layer3;
+    currentLayer = layer1;
 
     //  Create our tile selector at the top of the screen
     createTileSelector();
@@ -62,55 +74,55 @@ function create() {
 
     cursors = game.input.keyboard.createCursorKeys();
 
-    showLayersKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    layer1Key = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
-    layer2Key = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
-    layer3Key = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
+  //  showLayersKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  //  layer1Key = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
+  //  layer2Key = game.input.keyboard.addKey(Phaser.Keyboard.TWO);
+   // layer3Key = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
 
-    showLayersKey.onDown.add(changeLayer, this);
-    layer1Key.onDown.add(changeLayer, this);
-    layer2Key.onDown.add(changeLayer, this);
-    layer3Key.onDown.add(changeLayer, this);
+ //   showLayersKey.onDown.add(changeLayer, this);
+  //  layer1Key.onDown.add(changeLayer, this);
+ //   layer2Key.onDown.add(changeLayer, this);
+  //  layer3Key.onDown.add(changeLayer, this);
 
     console.log(layer1.index);
-    console.log(layer2.index);
-    console.log(layer3.index);
+  //  console.log(layer2.index);
+  //  console.log(layer3.index);
 
 }
 
-function changeLayer(key) {
+//function changeLayer(key) {
 
-    switch (key.keyCode)
-    {
-        case Phaser.Keyboard.SPACEBAR:
-            layer1.alpha = 1;
-            layer2.alpha = 1;
-            layer3.alpha = 1;
-            break;
+ //   switch (key.keyCode)
+  //  {
+  //      case Phaser.Keyboard.SPACEBAR:
+   //         layer1.alpha = 1;
+       //     layer2.alpha = 1;
+      //      layer3.alpha = 1;
+  //          break;
 
-        case Phaser.Keyboard.ONE:
-            currentLayer = layer1;
-            layer1.alpha = 1;
-            layer2.alpha = 0.2;
-            layer3.alpha = 0.2;
-            break;
+  //      case Phaser.Keyboard.ONE:
+  //          currentLayer = layer1;
+  //          layer1.alpha = 1;
+        //    layer2.alpha = 0.2;
+         //   layer3.alpha = 0.2;
+   //         break;
 
-        case Phaser.Keyboard.TWO:
-            currentLayer = layer2;
-            layer1.alpha = 0.2;
-            layer2.alpha = 1;
-            layer3.alpha = 0.2;
-            break;
+  //      case Phaser.Keyboard.TWO:
+ //           currentLayer = layer2;
+ //           layer1.alpha = 0.2;
+        //    layer2.alpha = 1;
+          //  layer3.alpha = 0.2;
+   //         break;
 
-        case Phaser.Keyboard.THREE:
-            currentLayer = layer3;
-            layer1.alpha = 0.2;
-            layer2.alpha = 0.2;
-            layer3.alpha = 1;
-            break;
-    }
+  //      case Phaser.Keyboard.THREE:
+   //         currentLayer = layer3;
+  //          layer1.alpha = 0.2;
+       //     layer2.alpha = 0.2;
+          //  layer3.alpha = 1;
+     //       break;
+ //   }
 
-}
+//}
 
 function pickTile(sprite, pointer) {
 
@@ -155,8 +167,8 @@ function update() {
 
 function render() {
 
-    game.debug.text('Current Layer: ' + currentLayer.name, 16, 550);
-    game.debug.text('1-3 Switch Layers. SPACE = Show All. Cursors = Move Camera', 16, 570);
+    game.debug.text('level 1: Beginner', 16, 500);
+  //  game.debug.text('1-3 Switch Layers. SPACE = Show All. Cursors = Move Camera', 16, 570);
 
 }
 
